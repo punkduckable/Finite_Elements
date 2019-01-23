7eb4fbb93458b5c5cebaf067041655195fab951e
Robert Stephany <robert.stephany@utexas.edu>
2019-01-22 19:51:10 -0600

    Updated Node class
    I rewrote a lot of the Node class. For one, I decided that the Bc, Has_BC, and Origional_Position arrays needed to be non-constant. I realized this when considering how I would implement this code when reading in nodes from a file. I would need an array of nodes wiht a runtime dependant length. Therefore, I would have to use a loop to set the BC's and Positions of each Node. As a result, it simply didn't make sense to use memeber initializer lists.
    
    To deal with these changes, I got rid of my "specify everything" constructor and instead created a "do nothing" constructor. I also wrote new "Set_Origional_Position" and "Set_BCs" methods. these methods do as their name would suggest. However, to add some level of security, I also created some new flag variables in the node class. These get set to true once the BCs or Position has been set (see Node.h:18, Node.cc:60).
    
    I also added a print method for the node class. This method simply prints out all relevant information for the current node.

--- Node.cc
+++ Node.cc
@@ -1,16 +1,79 @@
 #if !defined(NODE_SOURCE)
 #define NODE_SOURCE
 
-//////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Private methods
+
+void Node::Set_Current_Position(void) {
+  for(int i = 0; i < 3; i++)
+    if(Has_BC[i] == true)
+      Current_Position[i] = BC[i];
+    else
+      Current_Position[i] = Original_Position[i];
+} // void Node::Set_Current_Position(void) {
+
+
+////////////////////////////////////////////////////////////////////////////////
 // Setter methods
 
-void Node::Update_Position(const Array_3<double> New_Position) {
-  Current_Position = New_Position;
-} // void Node::Update_Position(const Array_3<double> New_Position) {
+void Node::Update_Position(const double New_Position_Component, const unsigned int component) {
+  // First, check that the component is 0, 1, or 2
+  if(component > 2) {
+    printf("Error! The component index must be 0, 1, or 2\n");
+    return;
+  } // if(component > 2) {
+
+  // Next, check if this component has a BC
+  if(Has_BC[component] == true) {
+    printf("Error! This node has a precribed boundary condition in the e%d direction\n", component+1);
+    return;
+  } // if(Has_BC[component] == true) {
+
+  // If both checks pass then update the current position
+  Current_Position[component] = New_Position_Component;
+} // void Node::Update_Position(const double New_Position_Component, const unsigned int component) {
+
+
+void Node::Set_Original_Position(const Array_3<double> Original_Position_In) {
+  /* Check if the Origional Position has already been set. This should only be
+  set once (when the node is first being created */
+  if(Original_Position_Has_Been_Set == false) {
+    // Set the Original Position using the passed Array
+    Original_Position = Original_Position_In;
+
+    // Original Position has been set
+    Original_Position_Has_Been_Set = true;
+
+    // Now, check if we're ready to set the Current_Position
+    if(Original_Position_Has_Been_Set == true && BCs_Have_Been_Set == true)
+      Set_Current_Position();
+  } // if(Original_Position_Has_Been_Set == false) {
+  else
+    printf("Error! This node already had its Original position set\n");
+} // void Node::Set_Original_Position(const Array_3<double> Original_Position_In) {
 
 
 
-//////////////////////////////////////////////////////////////////////////////
+void Node::Set_BCs(const Array_3<bool> Has_BC_In, const Array_3<double> BC_In) {
+  /* Check if the Node's Boundary Conditions have already been set. They should
+  only be set once when the Node is first being created. */
+  if(BCs_Have_Been_Set == false) {
+    // Set the Has_BC boolean array and the BC array using the inputs
+    Has_BC = Has_BC_In;
+    BC = BC_In;
+
+    // The BC's have been set.
+    BCs_Have_Been_Set = true;
+
+    // Now, check if we're ready to set the Current_Position
+    if(Original_Position_Has_Been_Set == true && BCs_Have_Been_Set == true)
+      Set_Current_Position();
+  } // if(BCs_Have_Been_Set == false) {
+  else
+    printf("Error! This node's BCs have already been set!\n");
+} // void Node::Set_BCs(const Array_3<bool> Has_BC_In, const Array_3<double> BC_In) {
+
+////////////////////////////////////////////////////////////////////////////////
 // Getter methods
 
 Array_3<double> Node::Get_Original_Position(void) const {
@@ -23,6 +86,16 @@ Array_3<double> Node::Get_Current_Position(void) const {
   return Current_Position;
 } // Array_3<double> Node::Get_Current_Position(void) const {
 
+
+////////////////////////////////////////////////////////////////////////////////
+// Other methods
+
+void Node::Print(void) {
+  printf(         "Original Position:            [%6.3lf, %6.3flf, %6.3lf]\n", Original_Position[0],  Original_Position[1], Original_Position[2]);
+  printf(         "Current Position :            [%6.3lf, %6.3flf, %6.3lf]\n", Current_Position[0] ,  Current_Position[1] , Current_Position[2] );
+  printf(         "Has BC           :            [%6d, %6d, %6d]\n"          , Has_BC[0]           ,  Has_BC[1]           , Has_BC[2]           );
+  printf(         "BC's             :            [%6.3lf, %6.3flf, %6.3lf]\n", BC[0]               ,  BC[1]               , BC[2]               );
+} // void Node::Print(void) {
 
 
 #endif
--- Node.h
+++ Node.h
@@ -6,44 +6,40 @@ class Node {
 friend class Element;
 
 private:
-  const Array_3<double> Original_Position;  // Material position of the node    Units : M
+  // Core members of the Node class
+  Array_3<double> Original_Position;        // Material position of the node    Units : M
   Array_3<double> Current_Position;         // Spatial position of the node     Units : M
-  const Array_3<bool> Has_BC;               // Has_BC[i] is true if the ith component of this node has a prescribed displacement BC
-  const Array_3<double> BC;                 // The Prescribed displacement BC's
+  Array_3<bool> Has_BC;                     // Has_BC[i] is true if the ith component of this node has a prescribed displacement BC
+  Array_3<double> BC;                       // The Prescribed displacement BC's
 
+
+  /* Flags. These are used so that the Original_Position, Has_BC, and BC variables
+  Can only be set once */
+  bool Original_Position_Has_Been_Set = false;
+  bool BCs_Have_Been_Set = false;
+
+  /* This method is called by the Set_Original_Position or Set_BCs functions if
+  both the BCs and Original_Position have been set */
+  void Set_Current_Position(void);
 public:
   //////////////////////////////////////////////////////////////////////////////
   // Constructors, Destructor
 
-  /* Constructor to explicitly set the Node's Original Position, and BC's. Note,
-  if no array is passed for Has_BC_In or BC_In then we will assume that this
-  node has no prescribed displacement */
-  Node(const Array_3<double> Original_Position_In, const Array_3<bool> Has_BC_In, const Array_3<double> BC_In)
-       : Original_Position(Original_Position_In), Has_BC(Has_BC_In), BC(BC_In) {
-
-    /* Set up the current position (the other private members are set up with
-    the member initialization list). Basically, if there's a prescribed
-    displacement then the current position is specified by that. Otherwise,
-    the original position is the current position. */
-    for(int i = 0; i < 3; i++)
-      if(Has_BC[i] == true)
-        Current_Position[i] = BC[i];
-      else
-        Current_Position[i] = Original_Position[i];
-  } // Node(const Array_3<double> Original_Position_In, const Array_3<bool> Has_BC_In, const Array_3<bool> BC_In)...
-
+  // Do nothing Default constructor
+  Node(void) {};
 
   // Do nothing destructor.
   ~Node(void) {};
-
-
 
   //////////////////////////////////////////////////////////////////////////////
   // Setter methods
 
   // Updates the current (spatial) position of the node
-  void Update_Position(const Array_3<double> New_Position);
+  void Update_Position(const double New_Position_Component, const unsigned int component);
 
+  // Set internal variables
+  void Set_Original_Position(const Array_3<double> Original_Position_In);
+  void Set_BCs(const Array_3<bool> Has_BC_In, const Array_3<double> BC_In);
 
 
   //////////////////////////////////////////////////////////////////////////////
@@ -51,6 +47,11 @@ public:
 
   Array_3<double> Get_Original_Position(void) const;
   Array_3<double> Get_Current_Position(void) const;
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Other public methods
+
+  void Print(void);
 }; // class Node {
 
 #endif
