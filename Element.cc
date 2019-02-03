e16da9c753c890d6dcd5ca0d436ba1b6a7dc143b
Robert Stephany <robert.stephany@utexas.edu>
2019-02-03 10:39:35 -0600

    Implemented Elements class (I think)
    I wrote and implemented the Elements class. I basically followed the implementation plan, but with a few important changes. For one, I quickly realized that the Elements class needs to have access to the ID array and Node_Array. I therefore added static members to do just this, as well as a function to set the up.
    
    By default, Node_Array and ID are set to NULL (see the top of Elements.cc). I can then use the value of these arrays to determine if Elements can be set up or not (see the explanation at Elements.cc:100).
    
    I explicitly disabled the = operator. I added a deep copy constructor (not sure if it'll ever be used but whatever).
    
    I still need to add members to set Ke, among other things. A printing function would also be useful. I also think that a function to access Local_Eq_Num_Tol_Global_Eq_num would be useful.
    
    I also want to add an Error file. I think that it's time for me to have actual error handling. I want to do this using a gigantic Enumerator that stores all possible error states. Void functions will then return an error state. If zero, the program will go on. If not, then the error state can be sent to an "Error_Handling_Function" to determine what went wrong (and hault the simulation)!
    
    This code compiles, but has not been tested (I need to write an element test function).

--- Element.cc
+++ Element.cc
@@ -4,4 +4,222 @@
 #include "Element.h"
 #include <stdio.h>
 
+////////////////////////////////////////////////////////////////////////////////
+// Set Elements static members
+
+unsigned Element::Num_Elements = 0;
+Node * Element::Node_Array = NULL;
+unsigned * Element::ID = NULL;
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Constructors, destructor
+
+// Deep copy constructor
+Element::Element(const Element & El) {
+  // Increment number of elements
+  Num_Elements++;
+
+  /* Check if El has its nodes set. If so, then deep copy Local_Eq_Num_To_Node,
+  Local_Eq_Num_To_Global_Eq_Num and Ke */
+  if(El.Nodes_Set == true) {
+    // Copy over non-dynamic members
+    for(int i = 0; i < 8; i++)
+      Node_List[i] = El.Node_List[i];
+
+    Nodes_Set = true;
+    Num_Local_Eq = El.Num_Local_Eq;
+
+
+    // Deep copy Local_Eq_Num_To_Node.
+    const unsigned int Twice_Num_Local_Eq = Num_Local_Eq*2;
+    Local_Eq_Num_To_Node = new unsigned[Twice_Num_Local_Eq];
+    for(int i = 0; i < Twice_Num_Local_Eq; i++)
+      Local_Eq_Num_To_Node[i] = El.Local_Eq_Num_To_Node[i];
+
+
+    // Deep copy Local_Eq_Num_To_Global_Eq_Num
+    Local_Eq_Num_To_Global_Eq_Num = new unsigned[Num_Local_Eq];
+    for(int i = 0; i < Num_Local_Eq; i++)
+      Local_Eq_Num_To_Global_Eq_Num[i] = El.Local_Eq_Num_To_Global_Eq_Num[i];
+
+
+    // Deep copy Ke
+    const unsigned int Num_Local_Eq_Squared = Num_Local_Eq*Num_Local_Eq;
+    Ke = new double[Num_Local_Eq_Squared];
+    for(int i = 0; i < Num_Local_Eq_Squared; i++)
+      Ke[i] = El.Ke[i];
+  } // if(El.Nodes_Set == true) {
+  else
+    Nodes_Set = false;
+} // Element::Element(const Element & El) {
+
+
+
+// Destructor
+Element::~Element(void) {
+  /* Check if nodes are set. If so then this element dynamically allocated
+  memory for Local_Eq_Num_To_Node, Local_Eq_Num_To_Global_Eq_Num, and Ke. We
+  need to free this memory before deleting the object. */
+  if(Nodes_Set) {
+    delete [] Local_Eq_Num_To_Node;
+    delete [] Local_Eq_Num_To_Global_Eq_Num;
+    delete [] Ke;
+  } // if(Nodes_Set) {
+
+  // Decrememnt number of elements
+  Num_Elements--;
+} // Element::~Element(void) {
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Other Element methods
+
+// Disabled = operator.
+Element & Element::operator=(const Element & El) {
+  printf("Error in Element & Element::operator=(const Element & El)\n");
+  printf("Element equality is not defined! Returning *this\n");
+  return *this;
+} // Element & Element::operator=(const Element & El) {
+
+
+
+// Set nodes
+void Element::Set_Nodes(const unsigned Node0_ID,
+                        const unsigned Node1_ID,
+                        const unsigned Node2_ID,
+                        const unsigned Node3_ID,
+                        const unsigned Node4_ID,
+                        const unsigned Node5_ID,
+                        const unsigned Node6_ID,
+                        const unsigned Node7_ID) {
+
+  /* First, check if the Element class has been set up.
+  The element class contains a few static members. One of these, the ID array,
+  points to the global ID array. Another, Node_Array, points to the
+  array of nodes used in this simulation. We need to have access to both of
+  these arrays to be able to set up the Element. Therefore, if these static
+  members have not been set and the user is trying to set this Element's nodes
+  then we need to thrown an error.
+
+  The Node_Array and ID array members are set to point to NULL when the
+  program begins (see the start of this file). Therefore, we can determine
+  if the Element class has been set up by checking if either of these static
+  members is NULL */
+  if(ID == NULL || Node_Array == NULL) {
+    printf("Error in void Element::Set_Nodes!\n");
+    printf("To set up an element, the ID and Node_Array static members must be set\n");
+    printf("Currently, ID = %p and Node_Array = %p. They are not set up. Aborting\n", ID, Node_Array);
+
+    return;
+  } // if(ID == NULL || Node_Array == NULL) {
+
+
+  /* If we've made it this far then the Element class has been set up. We can
+  therefore set the nodes.
+  To begin, set the Node_List using the passed Node_ID's */
+  Node_List[0] = Node0_ID;
+  Node_List[1] = Node1_ID;
+  Node_List[2] = Node2_ID;
+  Node_List[3] = Node3_ID;
+  Node_List[4] = Node4_ID;
+  Node_List[5] = Node5_ID;
+  Node_List[6] = Node6_ID;
+  Node_List[7] = Node7_ID;
+
+  // Nodes are now set
+  Nodes_Set = true;
+
+
+  //////////////////////////////////////////////////////////////////////////////
+  /* Determine the number of local equations. To do this, we cycle through
+  each node in the node list. We access the actual node using the Node_Array
+  For each node, we check if it has prescribed BC's in each component. */
+  Num_Local_Eq = 0;
+  for(int Node = 0; Node < 8; Node++) {
+    const unsigned Global_Node_Number = Node_List[Node];
+
+    for(int Component = 0; Component < 3; Component++) {
+      // Check the current node has a Precribed BC in the Component direction
+      if(Node_Array[Global_Node_Number].Has_BC[Component] == false)
+        Num_Local_Eq++;
+    } // for(int Component = 0; Component < 3; Component++) {
+  } // for(int Node = 0; Node < 8; Node++) {
+
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Allocate Local_Eq_Num_To_Node, Local_Eq_Num_To_Global_Eq_Num, and Ke
+
+  Local_Eq_Num_To_Node = new unsigned[2*Num_Local_Eq];
+  Local_Eq_Num_To_Global_Eq_Num = new unsigned[Num_Local_Eq];
+  Ke = new double[Num_Local_Eq*Num_Local_Eq];
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Set up Local_Eq_Num_To_Node, Local_Eq_Num_To_Global_Eq_Num
+
+  unsigned Eq_Num = 0;
+  for(int Node = 0; Node < 8; Node++) {
+    const unsigned Global_Node_Number = Node_List[Node];
+
+    for(int Component = 0; Component < 3; Component++) {
+      if(Node_Array[Global_Node_Number].Has_BC[Component] == false) {
+        // Set Local_Eq_Num_To_Global_Eq_Num
+        Local_Eq_Num_To_Global_Eq_Num[Eq_Num] = ID[3*Global_Node_Number + Component];
+
+        // Set Local_Eq_Num_To_Node
+        Local_Eq_Num_To_Node[2*Eq_Num + 0] = Global_Node_Number;
+        Local_Eq_Num_To_Node[2*Eq_Num + 1] = Component;
+
+        // Increment equation number
+        Eq_Num++;
+      } // if(Node_Array[Node_List[Node]].Has_BC[Component] == false) {
+    } // for(int Component = 0; Component < 3 Component++) {
+  } // for(int Node = 0; Node < 8; Node++) {
+} // void Element::Set_Nodes(const unsigned Node1_ID,
+
+
+
+// Get Node ID
+unsigned Element::Node_ID(const unsigned i) const {
+  // Check that i is within the bounds of the Node_List
+  if(i >= 8) {
+    printf("Error in unsigned Element::Node(const unsigned i)\n");
+    printf("You tried to access the %uth node but elements only have 8 nodes (indicies 0-7)\n", i);
+    return 0;
+  } // if(i >= 8) {
+
+  return Node_List[i];
+} // unsigned Element::Node_ID(const unsigned i) const {
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Friend functions
+
+void Set_Element_Static_Members(Node * Node_Array_Ptr, unsigned * ID_Ptr) {
+  /* This function is used to set up the Element class. We really only want to be
+  able to do this once. (doing so multiple times would lead to disaster).
+
+  The Node_Array and ID array static members are initialized to NULL (see
+  the top of this file). After being set, they will not be NULL.
+
+  Therefore, we can use the value of these pointers to determine if the element
+  class has been set up already. If it has, then we return an error */
+  if(Element::Node_Array != NULL || Element::ID != NULL) {
+    printf("Error in Set_Element_Static_Members(const Node * Node_Array_Ptr, const unsigned * ID)\n");
+    printf("The element class has already been set up!!!!!\n");
+    return;
+  } // if(Element::Node_Array != NULL || Element::ID != NULL) {
+
+  // Set Static members
+  Element::Node_Array = Node_Array_Ptr;
+  Element::ID = ID_Ptr;
+} // void Set_Element_Static_Members(Node * Node_Array_Ptr, unsigned * ID_Ptr) {
+
 #endif
--- Element.h
+++ Element.h
@@ -4,6 +4,77 @@
 #include "Node.h"
 
 class Element {
+private:
+  //////////////////////////////////////////////////////////////////////////////
+  // Static members
+  static unsigned Num_Elements;                  // Number of elements created
+  static Node * Node_Array;                      // Points to the Array of all the Nodes for the object being simulated
+  static unsigned * ID;                          // Points to the ID array
+
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Object specific private members
+
+  // The Global node numbers for the nodes associated with this Element.
+  bool Nodes_Set = false;
+  unsigned int Node_List[8];
+
+  // Number of local equations
+  unsigned int Num_Local_Eq;
+
+  /* Assembly arrays. Local_Eq_Num_To_Node is used to construct Ke while
+  Local_Eq_Num_To_Global_Eq_Num is used to map Ke into K. */
+  unsigned int * Local_Eq_Num_To_Node;                     // Stores the global node # and component associated with each local equation
+  unsigned int * Local_Eq_Num_To_Global_Eq_Num;            // Stores the global equation # associated with each local eqiation
+
+  // Local element stiffness matrix;
+  double * Ke;
+
+public:
+  //////////////////////////////////////////////////////////////////////////////
+  // Constructors, Destructor
+
+  // Default do nothing constructor
+  Element(void) { Num_Elements++; };
+
+  // Deep copy constructor
+  Element(const Element & El);
+
+  // Destructor
+  ~Element(void);
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Class methods
+
+  /* Explicit = operator method. C++ implicitly defines this method to simply
+  perform an member-by-member copy. The issue is that the Element class has
+  dynamically allocated members. Copying member-by-member would involve a
+  shallow copy. This could lead to disaster. Thus, I explicitly defined
+  this method to basically disable element equality. */
+  Element & operator=(const Element & El);
+
+  /* Set nodes.
+
+  This function sets Num_Local_Eq and allocates Local_Eq_Num_To_Node,
+  Local_Eq_Num_To_Global_Eq_Num, and Ke.
+
+  Local_Eq_Num_To_Node and Local_Eq_Num_To_Global_Eq_Num are set after being
+  allocated. */
+  void Set_Nodes(const unsigned Node0_ID,
+                 const unsigned Node1_ID,
+                 const unsigned Node2_ID,
+                 const unsigned Node3_ID,
+                 const unsigned Node4_ID,
+                 const unsigned Node5_ID,
+                 const unsigned Node6_ID,
+                 const unsigned Node7_ID);
+
+
+  unsigned Node_ID(const unsigned i) const;      // Returns the ID of the ith Node in the Node list
+
+  friend void Set_Element_Static_Members(Node * Node_Array_Ptr, unsigned * ID_Ptr);
 }; // class Element {
+
+void Set_Element_Static_Members(Node * Node_Array_Ptr, unsigned * ID_Ptr);
 
 #endif
