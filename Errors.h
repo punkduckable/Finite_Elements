65069617c3e2a0f5bb7b4ee53b8b66adce0a4f89
Robert Stephany <robert.stephany@utexas.edu>
2019-06-04 15:31:16 -0500

    Added (but did not implement) Element exceptions.
    I wrote the element exception classes + added descriptions. I have not implemented them/replaced the element error codes yet (that comes next).
    
    I wanted to leave a commit here, before switching over, just in case something terrible happens.

--- Errors.h
+++ Errors.h
@@ -13,7 +13,70 @@ void Handle_Error(const Node_Errors Error);
 
 
 
-// Element errors
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Element Exceptions
+
+/* Here I define the Element exception classes. Every Element exception class is
+derived from Element_Exception. As such, a Element_Exception handler will
+catch every type of Element exception. With that said, you can (and should)
+write handlers for specific types of Element exceptions.
+
+Note: All exception objects should be initialized with the message that you want to
+print out when the exception is caught.
+________________________________________________________________________________
+                        Description of the subclasses:
+
+Element_Not_Set_Up: This exception is thrown whenever the user runs a method
+that requires data that has not yet been set up. Many methods in the element
+class can not run until certian data paramaters (static  members, material data,
+Ke, etc...). It is possible to run these methods without this data, so an
+exception is thrown. An "Element_Not_Set_Up" exception basically says "you
+forgot to set up this element".
+
+Element_Already_Set_Up: On the flip side, this exception is thrown whenever
+the user tries to set a data member that has already been set (and should not
+be set again). The material, for example, should only be set once. An
+"Element_Already_Set_Up" exception basically says "you tried to set something
+that has already been set and should not be modified".
+
+Element_Bad_Determinant: This exception is thrown whenever an unphysical
+jacobian determinant (one that is <= 0) is computed. J should always be
+strictly positive, so getting an Element_Bad_Determinant exception probably
+means that something is wrong with the way that the nodes are ordered. */
+
+// Element Exception base class
+class Element_Exception {
+  private:
+    const std::string Error_Message;
+  public:
+    Element_Exception(const char* Error_Message) : Error_Message(Error_Message) {};
+    const char* what() const { return Error_Message.c_str(); }
+}; // class Element_Exception {
+
+
+
+class Element_Not_Set_Up : public Element_Exception {
+  public:
+    Element_Not_Set_Up(const char* Error_Message) : Element_Exception(Error_Message) {}
+}; // class Element_Not_Set_Up : public Element_Errors {
+
+
+
+class Element_Already_Set_Up : public Element_Exception {
+  public:
+    Element_Already_Set_Up(const char* Error_Message) : Element_Exception(Error_Message) {}
+}; // class Element_Already_Set_Up : public Element_Errors {
+
+
+
+class Element_Bad_Determinant : public Element_Exception {
+  public:
+    Element_Bad_Determinant(const char* Error_Message) : Element_Exception(Error_Message) {}
+}; // class Element_Bad_Determinant : public Element_Exception {
+
 enum class Element_Errors{SUCCESS,
                           STATIC_MEMBERS_NOT_SET, STATIC_MEMBERS_ALREADY_SET,
                           MATERIAL_NOT_SET, MATERIAL_ALREADY_SET,
@@ -25,8 +88,33 @@ enum class Element_Errors{SUCCESS,
 void Handle_Error(const Element_Errors Error);
 
 
+
+
+
+
 ////////////////////////////////////////////////////////////////////////////////
 // Matrix exceptions
+
+/* Here I define the Matrix class exceptions.  Every Matrix exception class is
+derived from Matrix_Exception. As such, a Matrix_Exception handler will
+catch every type of Matrix exception. With that said, you can (and should)
+write handlers for specific types of Matrix exceptions.
+
+Note: All exception objects should be initialized with the message that you want to
+print out when the exception is caught.
+________________________________________________________________________________
+                        Description of the subclasses:
+
+Matrix_Dimension_Mismatch: This exception is thrown whenever an operation
+involving two matricies is not possible because the two matricies are
+incompatible. This could happen if you're trying to compute M1*M2 when
+the number of columns in M1 does not equal the number of rows in M2, or if you
+try to set two matricies with different dimensions and/or memory layout equal
+to another (move = operator).
+
+Matrix_Index_Out_Of_Bounds: This exception is thrown whenever the user tries to
+access a component that is outside of the matrix (requested row or column index
+is too big).*/
 
 // Matrix Exception base class
 class Matrix_Exception {
@@ -37,10 +125,14 @@ class Matrix_Exception {
       const char* what() const { return Error_Message.c_str(); }
 }; // class Matrix_Exception {
 
+
+
 class Matrix_Dimension_Mismatch : public Matrix_Exception {
   public:
       Matrix_Dimension_Mismatch(const char* Error_Message) : Matrix_Exception(Error_Message) {}
 }; // class Matrix_Dimension_Mismatch : public Matrix_Exception {
+
+
 
 class Matrix_Index_Out_Of_Bounds : public Matrix_Exception {
   public:
