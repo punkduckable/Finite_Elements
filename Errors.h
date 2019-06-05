6a884423048c9e39560b721a6b6717dda215229a
Robert Stephany <robert.stephany@utexas.edu>
2019-06-05 17:51:44 -0500

    I added Node and Array ecxceptions (but did not implement them)
    I wrote exception classes for Nodes/Arrays. I have not, however, implemented these classes. Expect implementation in the next commit.
    
    Note: I decided to not make an exception class for the Node INDEX_OUT_OF_BOUNDS error code. The reason is that this error code gets thrown whenever the user tries to access an invalid component of an array... this is really an error with the Array class, so it should be treated as an Array exception, not a node one. Thus, I decided to make a set of Array_Exceptions (but have some node methods throw them).
    
    Expect all of this to be implemented in the next commit.

--- Errors.h
+++ Errors.h
@@ -3,7 +3,91 @@
 
 #include <string>
 
-// Node Errors
+////////////////////////////////////////////////////////////////////////////////
+// Array Exceptions
+
+/* Here I define the Array class exceptions. Each Array exception class is
+derived from the Array_Exception class. As such, a Array_Exception handler will
+catch every type of Array exception. With that said, you can (and should) write
+handlers for specific types of Array exceptions when possible.
+
+Note: all exception objects should be initialized with the message that you want
+to print when the exception is caught.
+________________________________________________________________________________
+                        Description of the subclasses:
+Array_Index_Out_Of_Bounds: This exception is thrown whenever the user tries to
+read or write to a component with an invalid index (the requested index is
+too big) */
+
+class Array_Exception {
+  private:
+    const std::string Error_Message;
+  public:
+    Array_Exception(const char * Error_Message) : Error_Message(Error_Message) {}
+    const char * what(void) const { return Error_Message.c_str(); }
+}; // class Array_Exception {
+
+
+class Array_Index_Out_Of_Bounds : public Array_Exception {
+  public:
+    Array_Index_Out_Of_Bounds(const char * Error_Message) : Array_Exception(Error_Message) {}
+}; // class Array_Index_Out_Of_Bounds {
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Node Exceptions
+
+/* Here I define the Node exception classes. Each Node exception class is
+derived from the Node_Exception class. As such, a Node_Exception handler will
+catch every type of Node exception. With that said, you can (and should) write
+handlers for specific types of Node exceptions when possible.
+
+Note: all exception objects should be initialized with the message that you want
+to print when the exception is caught.
+
+________________________________________________________________________________
+                        Description of the subclasses:
+
+Node_Not_Set_Up: This exception is thrown whenever the user runs a method
+that requires data that has not yet been set. Many methods in the node
+class can not run until the node's initial position has been set. It is
+impossible to run these methods without this data, so an exception is thrown.
+A "Node_Not_Set_Up" exception basically says "you forgot to set up this Node".
+
+Node_Already_Set_Up: On the flip side, this exception is thrown whenever
+the user tries to set a data member that has already been set (and should not
+be set again). The Node's boundary conditions, for example, should only be set
+once. A "Node_Already_Set_Up" exception basically says "you tried to set
+something that has already been set and should not be modified". */
+
+// Node exception base class
+class Node_Exception {
+  private:
+      const std::string Error_Message;
+  public:
+      Node_Exception(const char * Error_Message) : Error_Message(Error_Message) {}
+      const char * what(void) const { return Error_Message.c_str(); }
+}; // class Node_Exception {
+
+
+class Node_Not_Set_Up : public Node_Exception {
+  public:
+    Node_Not_Set_Up(const char * Error_Message) : Node_Exception(Error_Message) {};
+}; // class Node_Not_Set_Up : public Node_Exception {
+
+
+class Node_Already_Set_Up : public Node_Exception {
+  public:
+    Node_Already_Set_Up(const char * Error_Message) : Node_Exception(Error_Message) {}
+}; // class Node_Already_Set_Up : public Node_Exception {
+
+
+
+// Node errors
 enum class Node_Errors {SUCCESS,
                         NODE_NOT_SET_UP, NODE_ALREADY_SET_UP,
                         BC_ALREADY_SET,
@@ -19,22 +103,22 @@ void Handle_Error(const Node_Errors Error);
 ////////////////////////////////////////////////////////////////////////////////
 // Element Exceptions
 
-/* Here I define the Element exception classes. Every Element exception class is
-derived from Element_Exception. As such, a Element_Exception handler will
-catch every type of Element exception. With that said, you can (and should)
-write handlers for specific types of Element exceptions.
+/* Here I define the Element exception classes. Each Element exception class is
+derived from the Element_Exception class. As such, an Element_Exception handler
+will catch every type of Element exception. With that said, you can (and should)
+write handlers for specific types of Element exceptions when possible.
 
-Note: All exception objects should be initialized with the message that you want to
-print out when the exception is caught.
+Note: All exception objects should be initialized with the message that you want
+to print out when the exception is caught.
 ________________________________________________________________________________
                         Description of the subclasses:
 
 Element_Not_Set_Up: This exception is thrown whenever the user runs a method
-that requires data that has not yet been set up. Many methods in the element
+that requires data that has not yet been set up. Many methods in the Element
 class can not run until certian data paramaters (static  members, material data,
-Ke, etc...). It is possible to run these methods without this data, so an
+Ke, etc...). It is impossible to run these methods without this data, so an
 exception is thrown. An "Element_Not_Set_Up" exception basically says "you
-forgot to set up this element".
+forgot to set up this Element".
 
 Element_Already_Set_Up: On the flip side, this exception is thrown whenever
 the user tries to set a data member that has already been set (and should not
@@ -85,10 +169,10 @@ class Element_Bad_Determinant : public Element_Exception {
 ////////////////////////////////////////////////////////////////////////////////
 // Matrix exceptions
 
-/* Here I define the Matrix class exceptions.  Every Matrix exception class is
+/* Here I define the Matrix class exceptions. Each Matrix exception class is
 derived from Matrix_Exception. As such, a Matrix_Exception handler will
 catch every type of Matrix exception. With that said, you can (and should)
-write handlers for specific types of Matrix exceptions.
+write handlers for specific types of Matrix exceptions when possibl.
 
 Note: All exception objects should be initialized with the message that you want to
 print out when the exception is caught.
